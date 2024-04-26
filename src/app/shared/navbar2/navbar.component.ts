3cccfcf2715c9390083860bf8386ad09f2cb17f0
Alan Mendoza <amendoza@becompliance.es>
2024-04-25 20:05:38 -0600

    Ultimos cambios mios

--- src/app/shared/navbar2/navbar.component.ts
+++ src/app/shared/navbar2/navbar.component.ts
@@ -11,6 +11,39 @@ export class NavbarComponent2 implements OnInit {
     private toggleButton: any;
     private sidebarVisible: boolean;
     username: string | null = null;
+    query: string = '';
+    indexedContent: any[] = [
+        { keyword: 'inicio', url: '/#/dashboard' },
+        { keyword: 'resumen de proyectos', url: '/#/dashboard' },
+        { keyword: 'proyectos', url: '/#/dashboard' },
+        { keyword: 'activos', url: '/#/dashboard' },
+        { keyword: 'completados', url: '/#/dashboard' },
+        { keyword: 'resumen de actividades', url: '/#/dashboard' },
+        { keyword: 'actividades', url: '/#/dashboard' },
+        { keyword: 'lideres', url: '/#/dashboard' },
+        { keyword: 'líder', url: '/#/dashboard' },
+        { keyword: 'número de proyectos', url: '/#/dashboard' },
+        { keyword: 'agregar lider', url: '/#/dashboard' },
+        { keyword: 'editar perfil', url: '/#/mi-perfil' },
+        { keyword: 'nombre', url: '/#/mi-perfil' },
+        { keyword: 'apellidos', url: '/#/mi-perfil' },
+        { keyword: 'nombre de usuario', url: '/#/mi-perfil' },
+        { keyword: 'guardar cambios', url: '/#/mi-perfil' },
+        { keyword: 'contraseña actual', url: '/#/mi-perfil' },
+        { keyword: 'nueva contraseña', url: '/#/mi-perfil' },
+        { keyword: 'confirmar nueva contraseña', url: '/#/mi-perfil' },
+        { keyword: 'actualizar contraseña', url: '/#/mi-perfil' },
+        { keyword: 'proyectos', url: '/proyectos' },
+        { keyword: 'crear', url: '/proyectos' },
+        { keyword: 'registro de usuarios', url: '/#/gusuarios' },
+        { keyword: 'nombre', url: '/#/gusuarios' },
+        { keyword: 'apellido', url: '/#/gusuarios' },
+        { keyword: 'correo electrónico', url: '/#/gusuarios' },
+        { keyword: 'rol', url: '/#/gusuarios' },
+        { keyword: 'registrar usuario', url: '/#/gusuarios' },
+        { keyword: 'agregar usuario', url: '/#/gusuarios' },
+        { keyword: 'acciones', url: '/#/gusuarios' },
+    ];
 
     constructor(public location: Location, private element: ElementRef, private authService: AuthService) {
         this.sidebarVisible = false;
@@ -21,6 +54,18 @@ export class NavbarComponent2 implements OnInit {
         console.log(this.username);
         const navbar: HTMLElement = this.element.nativeElement;
         this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+    }
+
+    search(): void {
+        const keyword = this.query.toLowerCase().trim();
+        const result = this.indexedContent.find(item => item.keyword === keyword);
+
+        if (result) {
+            window.location.href = result.url;
+        } else {
+            // Manejar el caso en que no se encuentra ninguna coincidencia
+            console.log('No se encontraron resultados para la palabra clave:', keyword);
+        }
     }
 
     sidebarOpen() {
